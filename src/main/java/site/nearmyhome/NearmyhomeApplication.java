fc73ce6ea2522ec4943be2da2d6084628f2359ac
huhsiyoung <july433@naver.com>
2025-01-14 17:30:21 +0900

    first commit

---
+++ src/main/java/site/nearmyhome/NearmyhomeApplication.java
@@ -0,0 +1,13 @@
+package site.nearmyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearmyhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearmyhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/nearmyhome/NearmyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.nearmyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearmyhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
